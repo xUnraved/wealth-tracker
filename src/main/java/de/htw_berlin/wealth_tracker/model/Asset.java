e0c997df77fd10e96743acdd8306c46af6d58f44
xUnraved <153613575+xUnraved@users.noreply.github.com>
2026-06-14 10:47:48 +0200

    Update Asset.java

--- src/main/java/de/htw_berlin/wealth_tracker/model/Asset.java
+++ src/main/java/de/htw_berlin/wealth_tracker/model/Asset.java
@@ -1,19 +1,24 @@
 package de.htw_berlin.wealth_tracker.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import java.time.LocalDate;
 
-/**
- * Repräsentiert einen finanziellen Vermögenswert im Wealth Tracker.
- * Ein Asset kann eine Aktie, ETF, Krypto, Bargeld, Immobilie oder sonstiger Wert sein.
- */
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "assets")
 public class Asset {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String category;
     private double currentValue;
